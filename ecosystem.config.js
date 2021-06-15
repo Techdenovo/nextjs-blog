2d43c0fd1d3835e54f9c6ac1c696f89f089aa319
kalyan mazumder <kalyaninform@gmail.com>
2021-06-16 00:37:14 +0530

    ecosystem file added

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: 'nextjs-blog',
+        // cwd: ' /home/your-name/my-nextjs-project',
+        script: 'npm',
+        args: 'start',
+        // env: {
+        //   NEXT_PUBLIC_...: 'NEXT_PUBLIC_...',
+        // },
+      },
+      // optionally a second project
+  ],};
